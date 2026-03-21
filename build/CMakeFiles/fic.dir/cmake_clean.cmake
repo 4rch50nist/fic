495cc61fc059e180cf7df444e42f3b44cbeb8889
avs <adityavikramsinha19@gmail.com>
2026-03-21 17:24:31 +0530

    Added CMake for build system because Atharva is being oversmart.

---
+++ build/CMakeFiles/fic.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fic.dir/fic/Manifest/Manifest.cpp.o"
+  "CMakeFiles/fic.dir/fic/Manifest/Manifest.cpp.o.d"
+  "CMakeFiles/fic.dir/main.cpp.o"
+  "CMakeFiles/fic.dir/main.cpp.o.d"
+  "fic"
+  "fic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
